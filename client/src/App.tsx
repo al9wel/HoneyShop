4028d37a6fe9e033027534191f3a101801041046
Salem Alswil <sa.al9wel@gmail.com>
2025-11-20 18:18:41 +0300

    intilizing for deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,12 @@ const App = () => {
   return (
     <>
       <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
     </>
   )
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,21 @@
 import express from "express";
+import path from "path"
 import { ENV } from "./lib/env.js";
-const app = express()
+const app = express();
+const __dirname = path.resolve()
+
+app.get("/test", (req, res) => {
+    res.status(200).json({ msg: "test" })
+})
+
+
+// make the app ready for deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")))
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
+    })
+}
+
+
 app.listen(ENV.PORT, () => console.log("server is running in port : ", ENV.PORT))
